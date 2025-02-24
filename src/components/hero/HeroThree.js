371ee3f7b189873a6c78eddb609880bd73534b57
karan dhole <karan.dhole20@pccoepune.org.>
2025-02-24 12:46:55 +0530

    S

--- src/components/hero/HeroThree.js
+++ src/components/hero/HeroThree.js
@@ -58,7 +58,7 @@ export default class HeroThree extends React.Component {
                                     <div className="row">
                                         <div className="col-xl-12">
                                             <div className="main-slider__content">
-                                                <h2> "Everyone Has a Story And Every Story Is Worth Experiencing"</h2>
+                                                <h2> "Everyone Has a Story And Every Story Is Worth Experiencing"<br/><br/></h2>
                                                 
                                             </div>
                                         </div>
@@ -77,7 +77,7 @@ export default class HeroThree extends React.Component {
                                     <div className="row">
                                         <div className="col-xl-12">
                                             <div className="main-slider__content">
-                                            <h2>"We Are HERE to HEAR You"</h2>
+                                            <h2>"We Are HERE to HEAR You" <br/><br/></h2>
                                                 
                                             </div>
                                         </div>
